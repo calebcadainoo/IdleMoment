44fd7e2ec1c111c17cce2bd52c38c9897c733e3d
Caleb Crosby Adainoo <56970927+calebcadainoo@users.noreply.github.com>
2020-04-17 23:44:08 +0000

    first commit - without idle intelligence

---
+++ IdleMoment/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(83, 115)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "00:00:00"
+        '
+        'Timer1
+        '
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(290, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "00:00:00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(290, 59)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "00:00:00"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(380, 297)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ IdleMoment/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Drawing.Text
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    ' help check mouse activity
+    <StructLayout(LayoutKind.Sequential)> Structure LASTINPUTINFO 
+        <MarshalAs(UnmanagedType.U4)> Public cbSize As Integer
+        <MarshalAs(UnmanagedType.U4)> Public dwTime As Integer
+    End Structure
+    <DllImport("user32.dll")> Shared Function GetLastInputInfo(ByRef plii As LASTINPUTINFO) As Boolean
+
+    End Function
+
+    Dim idletime As Integer
+    Dim lastInputInf As New LASTINPUTINFO()
+    Public Function GetLastInputTime() As Integer
+        idletime = 0
+        lastInputInf.cbSize = Marshal.SizeOf(lastInputInf)
+        lastInputInf.dwTime = 0
+
+        If GetLastInputInfo(lastInputInf) Then
+            idletime = Environment.TickCount - lastInputInf.dwTime
+        End If
+
+        If idletime > 0 Then
+            Return idletime / 1000
+        Else : Return 0
+        End If
+    End Function
+
+    Private sumofidletime As TimeSpan = New TimeSpan(0)
+    Private LastLastIdletime As Integer = 0
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Black
+        Me.ForeColor = Color.White
+        Me.Opacity = 0.9
+        Me.TopMost = True
+        'Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+
+        Me.WindowState = FormWindowState.Normal
+        Me.StartPosition = FormStartPosition.Manual
+        With Screen.PrimaryScreen.WorkingArea
+            Me.SetBounds(.Left, .Top, .Width, .Height)
+        End With
+
+        Me.Bounds = Screen.GetBounds(Me)
+        'Me.MaximumSize = Screen.FromControl(Me).WorkingArea.Size
+        Me.WindowState = FormWindowState.Maximized
+
+        Label1.Width = Me.Width
+        Label1.Height = Me.Height
+
+        'Label1.BackColor = Color.Green
+        Label1.Location = New Point(0, 0)
+        Dim customFont As PrivateFontCollection = New PrivateFontCollection
+        customFont.AddFontFile("G:\FONTS\FontsUnpacked\BalooBhaina2-Regular.ttf")
+        Label1.Font = New Font(customFont.Families(0), 140)
+        Label1.AutoSize = False
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = TimeOfDay.ToString("hh • mm • ss")
+        'Label1.Text = TimeOfDay.ToString("hh • mm • ss tt")
+
+        Dim it As Integer = GetLastInputTime()
+        If LastLastIdletime > it Then
+            Label2.Text = "IDLE STATE CHANGED!"
+            sumofidletime = sumofidletime.Add(TimeSpan.FromSeconds(LastLastIdletime))
+            Label3.Text = "Sum of idle time: " & sumofidletime.ToString
+        Else
+            Label2.Text = GetLastInputTime()
+        End If
+        LastLastIdletime = it
+
+    End Sub
+End Class
